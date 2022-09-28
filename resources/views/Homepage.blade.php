f74d9e4ec17c3e8638b6351fc6cd37e613e40864
AlexBosio95 <alex.bosio1995@gmail.com>
2022-09-28 16:30:03 +0200

    cards rappresentation with DB source

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -4,9 +4,18 @@
 
 @section('contain')
 
-    <div>
+    <div class="d-flex flex-wrap justify-content-center">
         @foreach ($Movies as $movie)
-            <h1>{{$movie['title']}}</h1>
+            <div class="card m-2" style="width: 18rem;">
+                <img src="https://s.studiobinder.com/wp-content/uploads/2019/09/Movie-Genres-Types-of-Movies-List-of-Genres-and-Categories-Header-StudioBinder.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">{{$movie['title']}}</h5>
+                <p class="card-text">{{$movie['original_title']}}</p>
+                <p class="card-text">{{$movie['nationality']}}</p>
+                <p class="card-text">{{$movie['date']}}</p>
+                <span>{{$movie['vote']}}</span>
+                </div>
+            </div>
         @endforeach
     </div>
     
